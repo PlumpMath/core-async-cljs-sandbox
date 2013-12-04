2841046530a50b5749482e09f9039ee224d753ea
Dave Aitken <dave.aitken@gmail.com>
2013-12-04 17:50:18 +0000

    Finished tutorial

--- src/cljs/core_async_cljs_sandbox/core.cljs
+++ src/cljs/core_async_cljs_sandbox/core.cljs
@@ -6,25 +6,38 @@
   (:import [goog.net Jsonp]
            [goog Uri]))
 
-;(def wiki-search-url
-;  "http://en.wikipedia.org/w/api.php?action=opensearch&format=json&search=")
-;
-;(defn listen [el type]
-;  (let [out (chan)]
-;    (events/listen el type
-;                   (fn [e] (put! out e)))
-;    out))
-;
-;(defn jsonp [uri]
-;  (let [out (chan)
-;        req (Jsonp. (Uri. uri))]
-;    (.send req nil (fn [res] (put! out res)))))
-;
-;(defn query-url [q]
-;  (str wiki-search-url q))
-;
-;
-;(let [clicks (listen (dom/getElement "search") "click")]
-;  (go (.log js/console (<! (jsonp (query-url "cats"))))))
+(def wiki-search-url
+  "http://en.wikipedia.org/w/api.php?action=opensearch&format=json&search=")
 
-(.log js/console "hello")
+(defn listen [el type]
+  (let [out (chan)]
+    (events/listen el type
+                   (fn [e] (put! out e)))
+    out))
+
+(defn jsonp [uri]
+  (let [out (chan)
+        req (Jsonp. (Uri. uri))]
+    (.send req nil (fn [res] (put! out res)))
+    out))
+
+(defn query-url [q]
+  (str wiki-search-url q))
+;
+(defn user-query []
+  (.-value (dom/getElement "query")))
+
+(defn render-query [results]
+  (str "<ul>"
+       (apply str (for [result results] (str "<li>" result "</li>")))
+       "</ul>"))
+
+(defn init []
+  (let [clicks (listen (dom/getElement "search") "click")
+        results-view (dom/getElement "results")]
+    (go (while true
+          (<! clicks)
+          (let [[_ results] (<! (jsonp (query-url (user-query))))]
+            (set! (.-innerHTML results-view) (render-query results)))))))
+
+(init)
